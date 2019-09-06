b127f343964ee57312f088b3ed69b04c5e07ae94
fadelf <fadhelfaturrahman@gmail.com>
2019-09-06 13:30:39 +0700

    initial commit

--- src/components/views/WelcomeComponent.jsx
+++ src/components/views/WelcomeComponent.jsx
@@ -14,7 +14,7 @@ class WelcomeComponent extends Component {
                         This is a workforce management application, You can manage your employees <Link to="/employees">here</Link>.
                     </p>
                     <p>
-                        You can manage assignment for your employees <Link to="/assignments">here</Link>.
+                        You can manage job for your employees <Link to="/jobs">here</Link>.
                     </p>
                     <p>
                         <Button variant="primary">Learn more</Button>
